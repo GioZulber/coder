f11ffe1d97bc8c6c5d86621871ddde2c21b02772
giovanni.zulberti <giovannizulberti14@gmail.com>
2021-09-01 21:38:29 -0300

    Segunda Entrega

--- JS/pedidos.js
+++ JS/pedidos.js
@@ -16,34 +16,37 @@ let direccion = document.querySelector("#direccion");
 let talle = document.querySelector("#talle");
 let arrayPedidos = [];
 function validarDatos(e) {
-    let arrayPedidos = [];
     e.preventDefault();
     if (email.value == "" || marca.value == "" || modelo.value == "" || direccion.value == "") {
         alert("Error al ingresar los datos, complete todos los campos correctamente.")
     }
     arrayPedidos.push(new Pedidos(email.value, marca.value, modelo.value, direccion.value, talle.value));
-    arrayPedidos.forEach(pedido => {
-        let divPedidos = document.getElementById("sus-pedidos");
-        let ul = document.createElement("ul");
-        ul.innerHTML = `<li> Zapatilla: ${pedido.marca} ${pedido.modelo}</li>
-                        <li> Talle: ${pedido.talle}</li> 
-                        <li> Direccion: ${pedido.direccion}</li>`;
-        divPedidos.appendChild(ul)
-        console.log(`Correo Electronico: ${pedido.email}`);
-    });
+    localStorage.setItem("arrayPedidos", JSON.stringify(arrayPedidos))
+    rellenarDatos();
 }
-
+let pedidos = JSON.parse(localStorage.getItem("arrayPedidos"));
+function rellenarDatos(){
+    if (pedidos != null) {
+        pedidos.forEach(pedido => {
+            let divPedidos = document.getElementById("sus-pedidos");
+            let ul = document.createElement("ul");
+                ul.innerHTML = `<li> Zapatilla: ${pedido.marca} ${pedido.modelo}</li>
+                                <li> Talle: ${pedido.talle}</li> 
+                                <li> Direccion: ${pedido.direccion}</li>`;
+                divPedidos.appendChild(ul)
+                console.log(`Correo Electronico: ${pedido.email}`);
+            });
+    }
+}
 let buscadorFilter = document.querySelector("#buscadorFilter");
 buscadorFilter.addEventListener("submit", pedidosFilter);
 let pedidosBuscador = document.querySelector("#filtrados");
-
 function pedidosFilter(e) {
     e.preventDefault();
-   
     if (marca.value == "") {
         alert("Error al ingresar marca. Por favor ingrese una marca valida.")
     }else if (marca.value == "Jordan" || marca.value == "Adidas" || marca.value == "Nike" || marca.value == "Puma" ) {
-        let productoFiltrado= arrayPedidos.filter(elemento => elemento.marca == marca.value)
+        let productoFiltrado= pedidos.filter(elemento => elemento.marca == marca.value)
         productoFiltrado.forEach(filtrado => {
         let divFiltrados = document.getElementById("pedidosFiltrados");
         let ul = document.createElement("ul");
@@ -53,8 +56,5 @@ function pedidosFilter(e) {
         divFiltrados.appendChild(ul)
         console.log(`Correo Electronico: ${filtrado.email}`);
     })
-        
     } 
-        
-    
 }
