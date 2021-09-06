cf90a950f2c4992ab7f9aa0f0765492740233297
giovanni.zulberti <giovannizulberti14@gmail.com>
2021-09-06 03:20:52 -0300

    Agregue jQuery al proyecto

--- JS/main.js
+++ JS/main.js
@@ -1,60 +1,114 @@
-class Pedidos {
-    constructor(email, marca, modelo, direccion, talle) {
-        this.email = email;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.direccion = direccion;
-        this.talle = talle;
+//Definimos array general
+let listaTareas = [];
+
+//creamos el objeto tarea
+class Tarea{
+    constructor(id,nombre,descripcion){
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    toString(){
+        //devolvemos nuestro objeto en formato json
+        return JSON.stringify(this);
     }
 }
-let formulario = document.getElementById("formulario");
-formulario.addEventListener("submit", validarDatos);
-let email = document.querySelector("#exampleInputEmail1");
-let marca = document.querySelector("#marca");
-let modelo = document.querySelector("#modelo");
-let direccion = document.querySelector("#direccion");
-let talle = document.querySelector("#talle");
-let arrayPedidos = [];
-function validarDatos(e) {
-    let arrayPedidos = [];
-    e.preventDefault();
-    if (email.value == "" || marca.value == "" || modelo.value == "" || direccion.value == "") {
-        alert("Error al ingresar los datos, complete todos los campos correctamente.")
+//contador para usar de ID
+let contador = 0;
+
+function agregarTarea(evento){
+    //evento.preventDefault();
+    // forma 1 de traerlo
+    let nombre = $('#nombre');
+    //Forma 2
+    // let nombre = $('form #formTareas #nombre')
+    let descripcion = $('#descripcion');
+
+    // En vez de utilizar la propiedad value del input utilizamos un metodo val()
+    //Validamos que nombre no este vacio
+    if ( nombre.val() == "" ){
+        //si esta vacio sale y muestra un alert
+        alert("Ingresar Nombre valido");
+        return;
     }
-    arrayPedidos.push(new Pedidos(email.value, marca.value, modelo.value, direccion.value, talle.value));
-    arrayPedidos.forEach(pedido => {
-        let divPedidos = document.getElementById("sus-pedidos");
-        let ul = document.createElement("ul");
-        ul.innerHTML = `<li> Zapatilla: ${pedido.marca} ${pedido.modelo}</li>
-                        <li> Talle: ${pedido.talle}</li> 
-                        <li> Direccion: ${pedido.direccion}</li>`;
-        divPedidos.appendChild(ul)
-        console.log(`Correo Electronico: ${pedido.email}`);
-    });
-}
 
-let buscadorFilter = document.querySelector("#buscadorFilter");
-buscadorFilter.addEventListener("submit", pedidosFilter);
-let pedidosBuscador = document.querySelector("#filtrados");
+    //Validamos que descripcion no este vacio
+    if ( descripcion.val() == "" ){
+        //si esta vacio sale y muestra un alert
+        alert("Ingresar Descripcion valido");
+        return;
+    }
 
-function pedidosFilter(e) {
-    e.preventDefault();
-   
-    if (marca.value == "") {
-        alert("Error al ingresar marca. Por favor ingrese una marca valida.")
-    }else if (marca.value == "Jordan" || marca.value == "Adidas" || marca.value == "Nike" || marca.value == "Puma" ) {
-        let productoFiltrado= arrayPedidos.filter(elemento => elemento.marca == marca.value)
-        productoFiltrado.forEach(filtrado => {
-        let divFiltrados = document.getElementById("pedidosFiltrados");
-        let ul = document.createElement("ul");
-        ul.innerHTML = `<li> Zapatilla: ${filtrado.marca} ${filtrado.modelo}</li>
-                        <li> Talle: ${filtrado.talle}</li> 
-                        <li> Direccion: ${filtrado.direccion}</li>`;
-        divFiltrados.appendChild(ul)
-        console.log(`Correo Electronico: ${filtrado.email}`);
-    })
-        
-    } 
-        
+    console.log(`Nombre: ${nombre.val()} \n Descripcion: ${descripcion.val()}` );
+
+    //agregamos objeto Tarea
+    listaTareas.push(new Tarea(++contador, nombre.val(),descripcion.val()));
+
+    //recuperamos el elemento tabla
+    // let table = document.getElementById("tabla-tareas");
     
+    //creamos el Table ROW Element
+    // let tr = document.createElement("tr");
+    //Le cargamos los table data
+    // tr.innerHTML = `<td>${contador}</td>\n
+    // <td>${nombre.val()}</td>\n
+    // <td>${descripcion.val()}</td>`;
+    //agregamos a la estructura table
+    // table.appendChild(tr);
+
+    //METODOS NUEVO
+    //CON EL METODO APPEND SE AGREGA AL FINAL DE DONDE QUEREMOS
+    $('#tabla-tareas').append(`<tr><td>${contador}</td>\n
+                                <td>${nombre.val()}</td>\n
+                                <td>${descripcion.val()}</td></tr>`
+                                );
+    //CON EL METODO PREPEND SE AGREGA AL PRINCIPIO, ARRIBA DE DODNE QUEREMOS
+    // $('#tabla-tareas').prepend(`<tr><td>${contador}</td>\n
+    //                             <td>${nombre.val()}</td>\n
+    //                             <td>${descripcion.val()}</td></tr>`
+    // );
+    //Limpiamos lso valores de los inputs
+    nombre.val("");
+    descripcion.val("");    
+
 }
+
+
+//Funcion para guardar nuestro array en storage
+function guardarLista(){
+    localStorage.setItem("listaTareas",JSON.stringify(listaTareas));
+    console.log("La lista se ha guardado con " + listaTareas.length + " tareas:");
+    for(tarea of listaTareas){
+        console.log(tarea.toString());
+    }
+}
+
+let boton = document.getElementById("boton");
+
+boton.addEventListener("click",agregarTarea);
+
+//capturamos evento click de guardar
+let btnGuardar = document.getElementById("btnGuardar");
+btnGuardar.addEventListener("click",guardarLista);
+
+//Seleccion de distintos elementos utilizando un mismo selector
+let todosLosParrafos = $("p, tr");
+console.log(todosLosParrafos);
+
+//Usando seletores avanzados 
+let nombreTh = $('tr > th');
+
+console.log(nombreTh);
+
+//Cambiar algo en un grupo de nodos que traemos por selectores
+// ocultamos los elementos seleccionados
+/*
+$('h1, h2, p').hide();
+// Volvemos a visualizar los h1
+$('h1').show();
+*/
+
+//Camiar contenido en los html-
+//Esto cambia el texto en todos los Parrafos
+// $('p').html("Otro texto que se pega en todos los p");
--- JS/pedidos.js
+++ JS/pedidos.js
@@ -7,20 +7,20 @@ class Pedidos {
         this.talle = talle;
     }
 }
-let formulario = document.getElementById("formulario");
+let formulario = document.querySelector('#formulario');
 formulario.addEventListener("submit", validarDatos);
-let email = document.querySelector("#exampleInputEmail1");
-let marca = document.querySelector("#marca");
-let modelo = document.querySelector("#modelo");
-let direccion = document.querySelector("#direccion");
-let talle = document.querySelector("#talle");
+let email = $('#exampleInputEmail1');
+let marca = $('#marca');
+let modelo = $('#modelo');
+let direccion = $('#direccion');
+let talle = $('#talle');
 let arrayPedidos = [];
 function validarDatos(e) {
     e.preventDefault();
-    if (email.value == "" || marca.value == "" || modelo.value == "" || direccion.value == "") {
+    if (email.val() == "" || marca.val() == "" || modelo.val() == "" || direccion.val() == "") {
         alert("Error al ingresar los datos, complete todos los campos correctamente.")
     }
-    arrayPedidos.push(new Pedidos(email.value, marca.value, modelo.value, direccion.value, talle.value));
+    arrayPedidos.push(new Pedidos(email.val(), marca.val(), modelo.val(), direccion.val(), talle.val()));
     localStorage.setItem("arrayPedidos", JSON.stringify(arrayPedidos))
     rellenarDatos();
 }
@@ -28,32 +28,31 @@ let pedidos = JSON.parse(localStorage.getItem("arrayPedidos"));
 function rellenarDatos(){
     if (pedidos != null) {
         pedidos.forEach(pedido => {
-            let divPedidos = document.getElementById("sus-pedidos");
-            let ul = document.createElement("ul");
-                ul.innerHTML = `<li> Zapatilla: ${pedido.marca} ${pedido.modelo}</li>
-                                <li> Talle: ${pedido.talle}</li> 
-                                <li> Direccion: ${pedido.direccion}</li>`;
-                divPedidos.appendChild(ul)
+            $('#sus-pedidos').append(`<ul><li> Zapatilla: ${pedido.marca} ${pedido.modelo}</li>
+            <li> Talle: ${pedido.talle}</li> 
+            <li> Direccion: ${pedido.direccion}</li></ul>`);
                 console.log(`Correo Electronico: ${pedido.email}`);
             });
     }
+    email.val("");
+    marca.val("");
+    modelo.val("");
+    direccion.val("");
 }
 let buscadorFilter = document.querySelector("#buscadorFilter");
 buscadorFilter.addEventListener("submit", pedidosFilter);
-let pedidosBuscador = document.querySelector("#filtrados");
+let pedidosBuscador = $('#filtrados');
 function pedidosFilter(e) {
     e.preventDefault();
     if (marca.value == "") {
         alert("Error al ingresar marca. Por favor ingrese una marca valida.")
-    }else if (marca.value == "Jordan" || marca.value == "Adidas" || marca.value == "Nike" || marca.value == "Puma" ) {
+    }else if (marca.val() == "Jordan" || marca.val() == "Adidas" || marca.val() == "Nike" || marca.val() == "Puma" ) {
         let productoFiltrado= pedidos.filter(elemento => elemento.marca == marca.value)
         productoFiltrado.forEach(filtrado => {
-        let divFiltrados = document.getElementById("pedidosFiltrados");
-        let ul = document.createElement("ul");
-        ul.innerHTML = `<li> Zapatilla: ${filtrado.marca} ${filtrado.modelo}</li>
-                        <li> Talle: ${filtrado.talle}</li> 
-                        <li> Direccion: ${filtrado.direccion}</li>`;
-        divFiltrados.appendChild(ul)
+        $('#pedidosFiltrados').append(`<ul><li> Zapatilla: ${filtrado.marca} ${filtrado.modelo}</li>
+        <li> Talle: ${filtrado.talle}</li> 
+        <li> Direccion: ${filtrado.direccion}</li> </ul>`);
+        
         console.log(`Correo Electronico: ${filtrado.email}`);
     })
     } 
